d90f2c3ccecbd694bd469ac5ffc24f8417d561d5
Home <home@Homes-MacBook-Pro.local>
2020-02-09 13:58:42 -0600

    wireframe added

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+// let money = '1000';
+// let bet = '5';
+// let finalBet;
+
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+// let betOne = document.getElementById('bet1').addEventListener('click', (bet * 1));
+
+
+
+/*----- functions -----*/
+
