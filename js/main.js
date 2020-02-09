94078d9e4f88205c20e388592669e5cec219859a
Home <home@Homes-MacBook-Pro.local>
2020-02-09 14:33:56 -0600

    Added p tag for money

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 
 
 /*----- app's state (variables) -----*/
-// let money = '1000';
-// let bet = '5';
-// let finalBet;
+let money = 1000;
+let bet = 5;
+let finalBet;
 
 
 
@@ -13,9 +13,25 @@
 
 
 /*----- event listeners -----*/
-// let betOne = document.getElementById('bet1').addEventListener('click', (bet * 1));
+let betOne = document.getElementById('bet1').addEventListener('click', betTimesOne);
+let betThree = document.getElementById('bet3').addEventListener('click', betTimesThree);
+let betFive = document.getElementById('bet5').addEventListener('click', betTimesFive);
 
+let goSpin = document.getElementById('spin').addEventListener('click', playSpin)
 
 
 /*----- functions -----*/
+function betTimesOne() {
+    return finalBet = bet * 1;   
+};
+function betTimesThree() {
+    return finalBet = bet * 3;   
+};
+function betTimesFive() {
+    return finalBet = bet * 5;   
+};
+
+function playSpin() {
+    
+}
 
