90d47ee25863af29482b3708183188fbfcff134a
Home <home@homes-mbp.lan>
2020-02-11 19:58:25 -0600

    added win sound

--- js/main.js
+++ js/main.js
@@ -5,16 +5,13 @@ var icons = ['images/7-icon.jpeg', 'images/banana-icon.jpeg','images/bar-icon.jp
 let money;
 let bet = 5;
 let finalBet;
+let winSound = new Audio('sounds/winnerSound.m4a');
 
 /*----- cached element references -----*/
-const slot0 = document.getElementById('slot0');
-const slot1 = document.getElementById('slot1');
-const slot2 = document.getElementById('slot2');
-const bank = document.getElementById('bank');
-
 let img0 = document.getElementById('0');
 let img1 = document.getElementById('1');
 let img2 = document.getElementById('2');
+
 
 /*----- event listeners -----*/
 let betOne = document.getElementById('bet1').addEventListener('click', betTimesOne);
@@ -45,6 +42,11 @@ function init() {
     render();
 };
 
+// function preLoad() {
+//     winSound = loadSound("sounds/winnerSound.m4a");
+
+// };
+
 function playSpin() {
     money = money - finalBet;
     let idx = Math.floor(Math.random() * Math.floor(icons.length -1));
@@ -58,12 +60,14 @@ function playSpin() {
     render();
 };
 
+
+
 function bigWin() {
     if (img0.src === img1.src && img0.src === img2.src) {
         money = finalBet * 1000;
-        
-    
+        winSound.play();
     };
+    
     render();
 };
 
